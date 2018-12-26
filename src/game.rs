26fd1682e6733d6214b26bf44da845b20703b2e0
wonjin92 <kangwonjin92@gmail.com>
2018-12-26 21:53:35 +0900

    implement of snake.rs

--- src/game.rs
+++ src/game.rs
@@ -1,5 +1,9 @@
+use crate::snake::{Direction, Snake};
 use piston_window::*;
-use snake::Direction;
+use rand::{thread_rng, Rng};
+
+const MOVING_PERIOD: f64 = 0.1;
+const RESTART_TIME: f64 = 1.0;
 
 pub struct Game {
     food_exist: bool,
@@ -8,7 +12,8 @@ pub struct Game {
     width: i32,
     height: i32,
     game_over: bool,
-    waiting_time: f32,
+    waiting_time: f64,
+    snake: Snake,
 }
 
 impl Game {
@@ -21,6 +26,7 @@ impl Game {
             height,
             game_over: false,
             waiting_time: 0.0,
+            snake: Snake::new(3, 3),
         }
     }
 
@@ -29,20 +35,108 @@ impl Game {
             return;
         }
 
-        self.update_snake();
+        let dir = match key {
+            Key::Up => Some(Direction::Up),
+            Key::Down => Some(Direction::Down),
+            Key::Right => Some(Direction::Right),
+            Key::Left => Some(Direction::Left),
+            _ => Some(Direction::Up),
+        };
+
+        // 만약 내가 이후에 dir을 쓴다고 한다면 reference로 받아야 함
+        if &self.snake.head_direction().opposite() == dir.as_ref().unwrap() {
+            return;
+        }
+
+        self.update_snake(dir);
     }
 
-    pub fn draw(&self, c: &Context, g: &mut G2d) {}
+    pub fn draw(&self, c: &Context, g: &mut G2d) {
+        if self.food_exist {
+            // draw food
+        }
 
-    pub fn update(&mut self, delta_time: f64) {}
+        if self.game_over {
+            // draw game over            
+        }
 
-    fn check_eating() {}
+        // draw game board
 
-    fn check_if_snake_alive() {}
+        // draw snake
+    }
 
-    fn add_food() {}
+    pub fn update(&mut self, delta_time: f64) {
+        self.waiting_time += delta_time;
 
-    fn update_snake(&mut self) {}
+        if self.game_over {
+            if self.waiting_time > RESTART_TIME {
+                self.restart();
+            }
+            return;
+        }
 
-    fn restart(&mut self) {}
+        if !self.food_exist {
+            self.add_food();
+        }
+
+        if self.waiting_time > MOVING_PERIOD {
+            self.update_snake(None);
+        }
+    }
+
+    fn check_eating(&self) -> bool {
+        let (head_x, head_y) = self.snake.head_position();
+        head_x as u32 == self.food_x && head_y as u32 == self.food_y
+    }
+
+    fn check_if_snake_alive(&self) -> bool {
+        // next move에서 블럭이랑 겹치는지 확인
+        let (next_x, next_y) = self.snake.next_head();
+
+        // 뱀에 닿으면 false
+        if self.snake.overlap_tail(next_x, next_y) {
+            return false;
+        }
+
+        // 벽에 닿으면 false
+        next_x < 0 || next_y < 0 || next_x >= self.width || next_y >= self.height
+    }
+
+    fn add_food(&mut self) {
+        // 뱀에 안겹칠 때 까지 랜덤 생성
+        let mut rng = thread_rng();
+        let mut rand_x = rng.gen_range(1, self.width - 1);
+        let mut rand_y = rng.gen_range(1, self.height - 1);
+        while self.snake.overlap_tail(rand_x, rand_y) {
+            rand_x = rng.gen_range(1, self.width - 1);
+            rand_y = rng.gen_range(1, self.height - 1);
+        }
+
+        self.food_x = rand_x as u32;
+        self.food_y = rand_y as u32;
+        self.food_exist = true;
+    }
+
+    fn update_snake(&mut self, dir: Option<Direction>) {
+        if self.check_if_snake_alive() {
+            // 앞으로 움직이고, 음식을 먹었는지 확인
+            self.snake.move_forward(dir);
+            if self.check_eating() {
+                // 음식을 먹었다면 꼬리를 복구
+                self.snake.restore_tail();
+                self.food_exist = false;
+            }
+        }
+
+        self.waiting_time = 0.0;
+    }
+
+    fn restart(&mut self) {
+        self.food_exist = true;
+        self.food_x = 10;
+        self.food_y = 10;
+        self.game_over = false;
+        self.waiting_time = 0.0;
+        self.snake = Snake::new(3, 3);
+    }
 }
--- src/snake.rs
+++ src/snake.rs
@@ -1,7 +1,110 @@
+use std::collections::LinkedList;
 
+#[derive(PartialEq)]
 pub enum Direction {
-    UP,
-    DOWN,
-    RIGTH,
-    LEFT
+    Up,
+    Down,
+    Right,
+    Left,
+}
+
+impl Direction {
+    pub fn opposite(&self) -> Direction {
+        match self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Right => Direction::Left,
+            Direction::Left => Direction::Right,
+        }
+    }
+}
+
+#[derive(Clone)]
+struct Block {
+    x: i32,
+    y: i32,
+}
+
+pub struct Snake {
+    direction: Direction,
+    body: LinkedList<Block>,
+    tail: Option<Block>,
+}
+
+impl Snake {
+    pub fn new(x: i32, y: i32) -> Snake {
+        let mut body: LinkedList<Block> = LinkedList::new();
+        body.push_back(Block { x, y: y + 2 });
+        body.push_back(Block { x, y: y + 1 });
+        body.push_back(Block { x, y });
+        Snake {
+            direction: Direction::Right,
+            body,
+            tail: None,
+        }
+    }
+
+    pub fn next_head(&self) -> (i32, i32) {
+        let (head_x, head_y) = self.head_position();
+        let (next_x, next_y) = match self.head_direction() {
+            Direction::Down => (head_x, head_y + 1),
+            Direction::Up => (head_x, head_y - 1),
+            Direction::Right => (head_x + 1, head_y),
+            Direction::Left => (head_x - 1, head_y),
+        };
+        (next_x, next_y)
+    }
+
+    pub fn head_direction(&self) -> &Direction {
+        &self.direction
+    }
+
+    pub fn head_position(&self) -> (i32, i32) {
+        let head = self.body.front().expect("No head!");
+        (head.x, head.y)
+    }
+
+    pub fn move_forward(&mut self, direction: Option<Direction>) {
+        match direction {
+            Some(dir) => self.direction = dir,
+            None => ()
+        }
+
+        let head = self.body.front().expect("no snake body");
+        let new_block = match self.direction {
+            Direction::Down => Block {
+                x: head.x,
+                y: head.y + 1,
+            },
+            Direction::Up => Block {
+                x: head.x,
+                y: head.y + 1,
+            },
+            Direction::Left => Block {
+                x: head.x - 1,
+                y: head.y,
+            },
+            Direction::Right => Block {
+                x: head.x + 1,
+                y: head.y,
+            },
+        };
+        self.body.push_front(new_block);
+        // remove block
+        self.tail = self.body.pop_back();
+    }
+
+    pub fn overlap_tail(&self, next_x: i32, next_y: i32) -> bool {
+        for block in &self.body {
+            if block.x == next_x && block.y == next_y {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    pub fn restore_tail(&mut self) {
+        let lost_tail = self.tail.clone().expect("No Tail");
+        self.body.push_back(lost_tail);
+    }
 }
